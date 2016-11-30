ac46fcc9fe87b727e57f3fd91ffa57baedb74dbd
Kim Wilson <k.wilson541@gmail.com>
2016-11-30 10:53:52 +0000

    Added add link feature with tests

---
+++ spec/creating_links_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.feature 'adding a bookmark', :type => :feature do
+
+	scenario 'allows user to submit a new link' do
+		visit '/links/new'
+		fill_in 'url', :with => 'http://www.reddit.com'
+		fill_in 'title', :with => 'Reddit'
+		click_button 'Add Link'
+
+		within 'ul#links' do
+			expect(page).to have_content('Reddit')
+		end
+	end
+
+end
