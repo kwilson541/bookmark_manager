093a08f5203c3bd1be13f961727502fbec020312
Kim Wilson <k.wilson541@gmail.com>
2016-12-01 19:25:21 +0000

    Added user model and sign up feature

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'database_cleaner'
 gem 'rake'
+gem 'bcrypt'
 
 group :test do
   gem 'capybara'
--- app/app.rb
+++ app/app.rb
@@ -6,6 +6,9 @@ require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
+    enable :sessions
+    set :session_secret, 'super secret'
+
     get '/' do
         'Hello BookmarkManager!'
     end
@@ -34,6 +37,24 @@ class BookmarkManager < Sinatra::Base
         tag = Tag.first(name: params[:name])
         @links = tag ? tag.links : []
         erb :links
+    end
+
+    get '/users/new' do
+        erb :new_user
+    end
+
+    post '/users' do
+        user = User.create(email: params[:email], password: params[:password])
+        session[:user_id] = user.id
+        redirect '/links'
+    end
+
+    helpers do
+
+        def current_user
+            @current_user ||= User.get(session[:user_id])
+        end
+
     end
 
     # start the server if ruby file executed directly
